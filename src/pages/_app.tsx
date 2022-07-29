943056656be8356d88203d7406cb47bb49ec2849
Gleison Souza <gleison.lsouza@gmail.com>
2022-07-29 17:19:20 -0300

    Google Analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 import Layout from '../components/Layout/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageViews();
+
   return (
       <Layout>
+        <GoogleAnalytics />
         <Component {...pageProps} />
       </Layout>
     )
